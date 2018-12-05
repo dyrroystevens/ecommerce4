86ef1e5d350a7084351c997de3b07859d8ed6ff9
Alvaro Stevens <adyrroystevens@gmail.com>
2018-12-04 20:38:16 -0600

    venderlinea

---
+++ wp-content/themes/custom_sweet/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package custom_sweet
+ */
+
+?>
+
+	</div><!-- #content -->
+</div><!-- #page -->
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<p>Copyright 2018 | SWEET WORLD.(Alvaro Dyrroy Stevens Duran)</p>	
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+</body>
+</html>
